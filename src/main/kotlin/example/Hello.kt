786872f54950541ef0f9c6ef761e6310e4beb095
freewind <nowindlee@gmail.com>
2018-07-24 22:52:06 +0800

    demo

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,11 +1,28 @@
 package example
 
-import javafx.geometry.Pos
+import javafx.beans.binding.Bindings
+import javafx.beans.property.ReadOnlyObjectWrapper
+import javafx.beans.property.ReadOnlyProperty
+import javafx.beans.value.ObservableValue
 import tornadofx.*
 
 class HelloWorld : View() {
-    override val root = hbox {
-        label("Hello world")
+    override val root = vbox {
+        val input1 = textfield()
+        label(input1.textProperty().map { "$it!!" })
+        val input2 = textfield()
+        label(listOf(input1.textProperty(), input2.textProperty()).bindingMap { (t1, t2) -> "$t1 & $t2" })
+    }
+}
+
+fun <T : Any, K> List<ObservableValue<T>>.bindingMap(fn: (List<T>) -> K): ObservableValue<K> {
+    return Bindings.createObjectBinding({ fn(this.map { it.value }) }, this.toTypedArray())
+}
+
+fun <T, K> ReadOnlyProperty<T>.map(fn: (T) -> K): ReadOnlyProperty<K> {
+    val source = this
+    return ReadOnlyObjectWrapper<K>().apply {
+        source.addListener { _, _, newValue -> this.value = fn(newValue) }
     }
 }
 
@@ -14,8 +31,6 @@ class HelloWorldStyle : Stylesheet() {
         root {
             prefWidth = 400.px
             prefHeight = 400.px
-            alignment = Pos.CENTER
-            fontSize = 50.px
         }
     }
 }
